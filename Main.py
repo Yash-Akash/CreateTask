3916e92096d8456d4a507248706a15a2d425f474
Yash-Akash <38089199+Yash-Akash@users.noreply.github.com>
2018-04-06 08:52:01 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 from tkinter import *
 
-root = Tk()
-frame = Frame(root)
-frame.pack()
-label = Label(root, text = "test")
-label.pack()
-button1 = Button(frame, text = "Press", fg = "blue")
+frame = Tk()
+frame.geometry("750x500")
 
-button1.pack()
+potion = Button(frame, text = "Buy Potions", fg = "blue", width = 40, height = 10, command = "" )
+fullHeal = Button(frame, text = "Buy Full Heal", fg = "red", width = 40, height = 10, command = "" )
+potion.place(x = 40, y= 300)
+fullHeal.place(x = 400, y= 300)
 
+frame.mainloop()
 
-root.mainloop()
