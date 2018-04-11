ffa789a95c2634a10ae218b5c0e9df9ed878f234
Yash-Akash <38089199+Yash-Akash@users.noreply.github.com>
2018-04-11 08:34:26 -0400

    Update fight.py

--- fight.py
+++ fight.py
@@ -1,16 +1,17 @@
 from tkinter import *
 
 
-
 def fight():
     frameF = Tk()
-    frameF.geometry("300x300")
+    frameF.geometry("400x400")
     frameF.title("Fight!")
 
-    bf1 = Button(frameF, text="Attack!", fg="red", width=10, height=5)
-    bf1.place(x=110, y=200)
-    bf2 = Button(frameF, text="Back", fg="red", width=10, height=5)
-    bf2.place(x=250, y=200)
-
+    bf1 = Button(frameF, text="Fight!", bg="Firebrick3",font=('arial', 8, "bold"), width=10, height=5,)
+    bf1.place(x=130, y=200)
+    bf2 = Button(frameF, text="Quit", bg="snow", font=('arial', 8, "bold"), width=10, height=1, command = frameF.destroy())
+    bf2.place(x=300, y=300)
     frameF.mainloop()
 
+
+
+    #https://www.daniweb.com/programming/software-development/threads/477899/how-to-set-image-as-background-using-python-3-4
