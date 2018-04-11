478913de55e74801bc54f2a72f670b1a9e202c52
Yash-Akash <38089199+Yash-Akash@users.noreply.github.com>
2018-04-11 08:34:45 -0400

    Update MainLaunch.py

--- MainLaunch.py
+++ MainLaunch.py
@@ -1,24 +1,32 @@
-from tkinter import *
+import tkinter as tk
 from shop import *
 from fight import *
 from harvest import *
 
+
 def mainLaunch():
+
     frame = Tk()
-    frame.geometry("750x400")
+    frame.geometry("750x500")
     frame.title("Name of the game go here")
 
-    # buttons
-    b1 = Button(frame, text="Shop", fg="blue", width=30, height=7, command=shop)
-    b1.place(x=40, y=200)
-    b2 = Button(frame, text="Fight!", fg="red", width=30, height=7, command=fight)
-    b2.place(x=265, y=200)
-    b3 = Button(frame, text="Harvest", fg="orange", width=30, height=7, command=harvest)
-    b3.place(x=490, y=200)
-    b4 = Button(frame, text="Quit", fg="purple", width=10, height=1, command=quit)
-    b4.place(x=665, y=370)
+    bgImage = tk.PhotoImage(file="BGgradient.png")
+    w = bgImage.width()
+    h = bgImage.height()
+    frame.geometry("%dx%d+0+0" % (w, h))
+    mainpanel = tk.Label(frame, image=bgImage)
+    mainpanel.pack(side='top', fill='both', expand='yes')
+    mainpanel.image = bgImage
 
-    l1 = Label(frame, text="Insert future sample text here")
-    l1.place(x=130, y=30)
+    # buttons
+    b1 = Button(frame, text="Shop", bg="steel blue",font=('arial', 8, "bold"), width=30, height=7, command=shop)
+    b1.place(x=40, y=300)
+    b2 = Button(frame, text="Fight!", bg="Firebrick3",font=('arial', 8, "bold"), width=30, height=7, command=fight)
+    b2.place(x=265, y=300)
+    b3 = Button(frame, text="Harvest", bg="coral3",font=('arial', 8, "bold"), width=30, height=7, command=harvest)
+    b3.place(x=490, y=300)
+    b4 = Button(frame, text="Quit", bg="snow",font=('arial', 8, "bold"), width=10, height=1, command=quit)
+    b4.place(x=665, y=470)
+
 
     frame.mainloop()
