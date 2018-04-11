a59ba2a4547678196ef995171adaec6c821878c2
Yash-Akash <38089199+Yash-Akash@users.noreply.github.com>
2018-04-11 07:47:03 -0400

    Update fight.py

--- fight.py
+++ fight.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
+
+
 def fight():
     frameF = Tk()
     frameF.geometry("300x300")
     frameF.title("Fight!")
 
+    bf1 = Button(frameF, text="Attack!", fg="red", width=10, height=5)
+    bf1.place(x=110, y=200)
+    bf2 = Button(frameF, text="Back", fg="red", width=10, height=5)
+    bf2.place(x=250, y=200)
 
-
-
+    frameF.mainloop()
 
